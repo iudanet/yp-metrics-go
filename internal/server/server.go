bf8e91d2a6f11474486d7e647022b99a94454a92
Aleksandr Chudakov <chudo88@gmail.com>
2025-05-01 13:41:09 +0300

    Move HTML template to separate file

--- internal/server/server.go
+++ internal/server/server.go
@@ -110,31 +110,7 @@ func (s *service) GetIndex(w http.ResponseWriter, r *http.Request) {
 		Gauges:   gauges,
 	}
 
-	// Парсим HTML-шаблон
-	tmpl := template.Must(template.New("index").Parse(`
-  <!DOCTYPE html>
-  <html lang="en">
-  <head>
-   <meta charset="UTF-8">
-   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>Метрики</title>
-  </head>
-  <body>
-   <h1>Метрики Counters</h1>
-   <ul>
-   {{range $key, $value := .Counters}}
-    <li>{{$key}}: {{$value}}</li>
-   {{end}}
-   </ul>
-   <h1>Метрики Gauges</h1>
-   <ul>
-   {{range $key, $value := .Gauges}}
-    <li>{{$key}}: {{printf "%4.3f" $value}}</li>
-   {{end}}
-   </ul>
-  </body>
-  </html>
- `))
+	tmpl := template.Must(template.New("index").Parse(indexTemplate))
 
 	// Рендерим шаблон
 	if err := tmpl.Execute(w, data); err != nil {
